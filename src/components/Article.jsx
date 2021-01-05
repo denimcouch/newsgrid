ee9e65334a514b74df87778ca9983871816ad977
denimcouch <alexmata92@ymail.com>
2021-01-05 00:36:19 -0600

    made HomeArticlesContainer more readable

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
+import React from "react";
 
-export default function Article (props) {
+export default function Article(props) {
   const readCategory = () => {
     switch (props.category) {
-      case 'entertainment':
+      case "entertainment":
         return "category category-ent";
-      case 'sports':
+      case "sports":
         return "category category-sports";
-      case 'technology':
+      case "technology":
         return "category category-tech";
       default:
-        return "category"
+        return "category";
     }
-  }
+  };
   return (
     <article className="card">
-      <img src={props.img} alt=""/>
+      <img src={props.img} alt="" />
       <div className={readCategory()}>{props.category}</div>
       <h3>
         <a href="#">Lorem ipsum dolor sit amet.</a>
       </h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, iusto. Voluptatem accusamus eum corrupti harum illo vero, rerum quidem omnis doloribus aperiam ipsam sit quae.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, iusto.
+        Voluptatem accusamus eum corrupti harum illo vero, rerum quidem omnis
+        doloribus aperiam ipsam sit quae.
+      </p>
     </article>
-  )
+  );
 }
--- src/containers/HomeArticlesContainer.jsx
+++ src/containers/HomeArticlesContainer.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Article from "../components/Article";
 
-export default function HomeArticlesContainer({articlePics}) {
+export default function HomeArticlesContainer({ articlePics }) {
+
+  const createArticles = () => {
+    return articlePics.map((imgObj) => (
+      <Article
+        className="home-article"
+        img={imgObj.img}
+        category={imgObj.category}
+      />
+    ));
+  };
+  
   return (
     <section id="home-articles" className="py-2">
       <div className="container">
         <h2>Editor's Pick</h2>
-        <div className="articles-container">
-          {articlePics.map( imgObj => <Article img={imgObj.img} category={imgObj.category}/>)}
-        </div>
+        <div className="articles-container">{createArticles()}</div>
       </div>
     </section>
   );
