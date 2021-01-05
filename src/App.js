fa2bce46afe4684f68aa2cc342cfee28142ef3af
denimcouch <alexmata92@ymail.com>
2021-01-05 00:22:29 -0600

    articles populate based on number of photos

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import ShowcaseHeader from './components/ShowcaseHeader'
 import HomeArticlesContainer from './containers/HomeArticlesContainer'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import ent1 from "./image_resources/articles/ent1.jpg";
+import ent2 from "./image_resources/articles/ent2.jpg";
+import sports1 from "./image_resources/articles/sports1.jpg";
+import tech1 from "./image_resources/articles/tech1.jpg";
+import tech2 from "./image_resources/articles/tech2.jpg";
 
 library.add(fab)
 
 function App() {
+  const articlePics = [ent1, ent2, sports1, tech1, tech2]
   return (
     <div className="app">
       <NavBar />
       <ShowcaseHeader />
-      <HomeArticlesContainer />
+      <HomeArticlesContainer articlePics={articlePics} />
     </div>
   );
 }
--- src/containers/HomeArticlesContainer.jsx
+++ src/containers/HomeArticlesContainer.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import Article from '../components/Article'
-import ent1 from '../image_resources/articles/ent1.jpg'
+import React from "react";
+import Article from "../components/Article";
+import ent1 from "../image_resources/articles/ent1.jpg";
 
-
-export default function HomeArticlesContainer () {
-  return(
+export default function HomeArticlesContainer({articlePics}) {
+  return (
     <section id="home-articles" className="py-2">
       <div className="container">
         <h2>Editor's Pick</h2>
-      <div className="articles-container">
-        <Article img={ent1}/>
-      </div>
+        <div className="articles-container">
+          {articlePics.map( img => <Article img={img}/>)}
+        </div>
       </div>
     </section>
-  )
+  );
 }
