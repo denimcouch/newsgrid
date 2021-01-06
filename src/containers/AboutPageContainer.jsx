a06a1ef0fb854a566d5eef2dc97d0df6b42a2835
denimcouch <alexmata92@ymail.com>
2021-01-06 16:29:59 -0600

    about page built

--- src/containers/AboutPageContainer.jsx
+++ src/containers/AboutPageContainer.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-
+import AboutInfo from '../components/AboutInfo'
+import AboutAside from '../components/AboutAside'
 
 export default function AboutPageContainer() {
   return (
-    <section id="about-page" className="py-2">
-      <div>
-        <h1>About</h1>
+    <section id="about">
+      <div className="container">
+        <div className="page-container">
+          <AboutInfo/>
+          <AboutAside/>
+        </div>
       </div>
     </section>
   );
